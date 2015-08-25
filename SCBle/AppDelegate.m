23d7014f5ac0fef6e20cb91b622ced1df35cada4
matt <297377344@qq.com>
2015-08-25 22:22:54 +0800

    提交错误内容

--- SCBle/AppDelegate.m
+++ SCBle/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"new");
+    NSLog(@"err msg");
     return YES;
 }
 
